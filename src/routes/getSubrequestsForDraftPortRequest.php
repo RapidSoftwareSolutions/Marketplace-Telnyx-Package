2960ae75a01c1d19f03d6275e7c721c3dc409a8f
Stefan Skliarov <serg.osipchuk@rapidapi.com>
2017-08-16 16:34:18 +0300

    method changed to POST

--- src/routes/getSubrequestsForDraftPortRequest.php
+++ src/routes/getSubrequestsForDraftPortRequest.php
@@ -15,7 +15,7 @@ $app->post('/api/Telnyx/getSubrequestsForDraftPortRequest', function ($request, 
     $query_str = $settings['api_url'] . 'origination/porting/draft_port_requests/' . $post_data['args']['draftPortRequestId'] . '/sub_requests';
     $params = [
     ];
-    $result = \Models\ApiRequestFacade::makeRequest($params, $post_data, $query_str);
+    $result = \Models\ApiRequestFacade::makeRequest($params, $post_data, $query_str, 'POST', 'json');
     return $response->withHeader('Content-type', 'application/json')->withStatus(200)->withJson($result);
 
 });
